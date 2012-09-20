883f50dd42eb04b171a74c5080493b05a1dbd261
Tomas <tomas@nesys.com.ar>
2012-09-19 22:14:55 -0300

    Se deja planteado el ejercicio 4.

---
+++ lib/IR/General4.pm
@@ -0,0 +1,245 @@
+package IR::General4;
+
+#use strict;
+use warnings;
+
+use Switch;
+
+use utf8;
+
+our $VERSION = '1.0';
+
+use base 'Exporter';
+
+our @EXPORT = qw(palabras_vacias preparar_linea tokenizar limpiar_token token_valido);
+
+=head1 NAME
+
+IR::General4 - Metodos generales y utiles para IR
+
+=head1 SYNOPSIS
+
+  use IR::General3;
+  @stop_words = palabras_vacias();
+  $linea = preparar_linea($linea);
+
+=head1 DESCRIPTION
+
+Modulo de funciones generales y utiles para manejo de IR
+
+=head2 Functions
+
+Las siguientes funciones son exportadas por defecto al invocar el modulo.
+
+=head3 palabras_vacias
+
+    @stop_words = palabras_vacias();
+
+Retorna un arreglo con las palabras que se consideran vacias
+
+=head3 preparar_linea
+
+    $linea = preparar_linea($linea)
+
+Se encarga de dejar la linea del archivo lista para ser tokenizada en un paso posterior
+
+=cut
+
+### VARIABLES DEL MODULO
+
+# Tratamiento de palabras vacias
+$aceptar_palabras_vacias = 0; # 0: False; 1: True;
+@palabras_vacias = palabras_vacias();
+
+$tamano_minimo = 1;
+$tamano_maximo = 24; # Estudiar un poco mas
+
+# Definicion de la funcion palabras_vacias().
+sub palabras_vacias {
+    
+    open (FH, "lib/IR/stopwords.txt") or die "No se pudo abrir el archivo: $!";
+    
+    while($termino = <FH>) {
+        
+        chomp($termino);
+        
+        push(@palabras_vacias, $termino);
+        
+    }
+    
+    close FH or die "No se pudo cerrar el archivo: $!"; 
+    
+    @otros_simbolos = [qw(x n a d p e k o stm_aix raquo i var if font function size 0 1 2 3 4 5 6 7 8 9 " ' “ ‘ ’ ´ ¨ , ; : = - \) \( / \\ [ ] { } | & ? ¿ ! * < >)];
+    
+    return (@palabras_vacias,@otros_simbolos);
+    
+}
+
+# Definicion de la funcion preparar_linea()
+# Esta funcion limpia la linea y la deja preparada para tokenizar
+sub preparar_linea {
+    
+    my $linea = shift;
+    
+    chomp($linea);
+    
+    # Eliminamos caracteres que no nos interesa procesar
+    $linea =~ tr/"+#%.<>\\\/“” ´'·¨‘’\(\)[]{};:!¡¿?*\|º°~¦§ª=&_/                                               /;
+    
+    utf8::decode($linea);
+    
+    # Normalizamos caracteres
+    $linea =~ tr/ñÑóíáéúüÓÍÁÉÚÜàèìòùÀÈÌÒÙâêîôûÂÊÎÔÛäëïöÄËÏÖåÅãÃõÕ/nNoiaeuuOIAEUUaeiouAEIOUaeiouAEIOUaeioAEIOaAaAoO/;
+    
+    # Pasamos todo a minuscula
+    $linea =~ tr/A-Z/a-z/;
+    
+    # Si hay caracteres no numericos con , y sin espacio, eliminamos la , por un espacio
+    $linea =~ s/([a-z]+),/$1 /g;
+    $linea =~ s/,([a-z]+)/ $1/g;
+    
+    # Si encontramos numeros "pegados" a texto, lo separamos, y visceversa
+    #~ $linea =~ s/([0-9])([a-z])/$1 $2/g;
+    #~ $linea =~ s/([a-z])([0-9])/$1 $2/g;
+    # No usar, rompemos url, mails, etc..
+    
+    return $linea;
+    
+}
+
+# Definicion de la funcion tokenizar()
+# Esta funcion transforma una linea en tokens especificos
+sub tokenizar {
+    
+    my $linea = shift;
+    
+    # Las palabras se separan por espacios
+    @tokens = split(" ", $linea);
+    
+    return @tokens;
+    
+}
+
+# Definicion de limpiar_token()
+# Funcion que se encarga de borrar todo lo que no se acepta como token. Devolvera el token listo para ser usado en el indice
+sub limpiar_token {
+    
+    my $token = shift;
+    
+    # Si termina o empieza con punto u otro caracter que tmb lo reemplaze
+    $token =~ s/\.+$/ /g;
+    $token =~ s/^\.+/ /g;
+    $token =~ s/\,+$/ /g;
+    $token =~ s/^\,+/ /g;
+    $token =~ s/\++$/ /g;
+    $token =~ s/^\++/ /g;
+    $token =~ s/\$+$/ /g;
+    $token =~ s/^\$+/ /g;
+    $token =~ s/\%+$/ /g;
+    $token =~ s/^\%+/ /g;
+    $token =~ s/\-+$/ /g;
+    $token =~ s/^\-+/ /g;
+    
+    # Eliminamos todos los espacios
+    $token =~ s/\s//g;
+    
+    return $token;
+    
+}
+
+# Definicion de token_valido()
+#   Esta funcion tiene como mision decidir que considera un token y que no
+#   Establecemos en el switch todas las estructuras validas, y al final, algunas
+#       condiciones adicionales de verificacion.
+sub token_valido {
+    
+    my $token = shift;
+    
+    $token_valido = 0;
+    
+    if ( (not es_palabra_vacia($token)) and tiene_largo_adecuado($token) ) {
+    
+        switch ($token) {
+            
+            case m/[0-9]+[\.|\,]*[0-9]*[\.|\,]*[0-9]*/  { 
+                
+                # Formato de numero
+                $token_valido = 1;
+                
+            }
+            
+            case m/[a-z]+/ {
+                
+                # Formato palabras
+                $token_valido = 1;
+                
+            }
+        
+        }
+    
+    }
+    
+    return $token_valido;
+    
+}
+
+sub es_palabra_vacia {
+    
+    my $token = shift;
+    
+    if( $aceptar_palabras_vacias ) {
+        
+        return 0;
+        
+    } else {
+        
+        if ( $token ~~ @palabras_vacias ) {
+            
+            return 1;
+            
+        } else {
+            
+            return 0;
+            
+        }
+        
+    }
+    
+}
+
+sub tiene_largo_adecuado {
+    
+    my $token = shift;
+    
+    #~ # Solo se usa a fin de investigar los caracteres largos. Es poco performante asi que comentar en produccion
+    #~ if ( length($token) > $tamano_maximo ) {
+        #~ 
+        #~ open(OUT, ">>salidas/tokens_largos.txt");
+        #~ 
+        #~ print OUT "$token\n";
+        #~ 
+        #~ close(OUT);
+        #~ 
+    #~ }
+    
+    if ( ( length($token) > $tamano_minimo ) and ( length($token) < $tamano_maximo ) ) {
+        
+        return 1;
+        
+    } else {
+        
+        return 0;
+        
+    }
+        
+}
+
+=head1 AUTHOR
+
+Tomas Delvechio <tomas@nesys.com.ar>
+Modulo escrito gracias a la documentacion de http://en.wikipedia.org/wiki/Perl_module
+
+=cut
+
+# Todo modulo de perl debe terminar en 1 (True)
+1;
---
+++ tp_2_ej_4.pl
@@ -0,0 +1,206 @@
+#!/usr/bin/perl
+system(clear);
+
+#use encoding 'utf8';
+use lib('lib/');
+use IR::General4;
+
+#################
+### Variables ###  
+#################
+
+$directorio_coleccion = './colecciones/T12012-qm';
+
+$archivo_terminos = './salidas/ejercicio3/terminos.txt';
+
+$archivo_estadisticas = './salidas/ejercicio3/estadisticas.txt';
+
+$archivo_frecuencias = './salidas/ejercicio3/frecuencias.txt';
+
+#####################
+### Procesamiento ###
+#####################
+
+# Abrimos el directorio
+opendir (dir, $directorio_coleccion) or die "No se pudo abrir";
+
+# Recorremos cada archivo
+while( $file = readdir( dir ) ) {
+    
+    # Ignoramos las referencias de unix a si mismo y al padre
+    if( $file ne "." and $file ne ".." ) {
+        
+        # Abre el archivo
+        open (IN,"$directorio_coleccion/$file");
+        binmode (IN, "utf8");
+        print "Analizando archivo: $file\n";
+        
+        $estadisticas{"Cantidad de documentos procesados"} += 1;
+        
+        # Para cada linea del archivo
+        while( $linea = <IN> ) {
+            
+            # Preparamos la linea para generar los token
+            $linea = preparar_linea($linea);
+            #print $linea;
+            # Tokenizamos la linea
+            @tokens = tokenizar($linea);
+            
+            # Procesamos cada uno de los tokens de la linea
+            foreach $palabra ( @tokens ) {
+                
+                $palabra = limpiar_token($palabra);
+                
+                if ( token_valido( $palabra ) ) {
+                    
+                    # Calculo del TF
+                    $frecuencias{$palabra}{"TF"} += 1;
+                    #print "$palabra";
+                    # Recogemos estadisticas de cantidad de terminos por documento
+                    $estadisticas{"cantidad_terminos_documentos"}{$file}{$palabra} += 1;
+                    
+                    # Para despues calcular el DF
+                    $frecuencias{$palabra}{$file} = 1;
+                    
+                }
+                
+            }
+            
+        }
+        
+        close(IN);
+    
+    }
+
+}
+
+#########################
+### Pos-procesamiento ###
+#########################
+
+# Armamos un arreglo con las claves ordenadas por la TF en forma descendente, para el punto c) del TP
+@terminos_ordenados_por_frecuencias = sort { $frecuencias{$b}{"TF"} <=> $frecuencias{$a}{"TF"} } keys %frecuencias;
+
+$estadisticas{"Cantidad de terminos extraidos"} = scalar(keys %frecuencias);
+
+$estadisticas{"Promedio de terminos por documento"} =  int($estadisticas{"Cantidad de terminos extraidos"} / $estadisticas{"Cantidad de documentos procesados"});
+
+# Calculo del DF
+foreach $termino (keys %frecuencias) {
+    
+    $df = scalar(keys %{$frecuencias{$termino}});
+    
+    # Ignoramos el indice TF que no corresponde
+    $df = $df - 1;
+    
+    $estadisticas{"Largo promedio de un termino"} += length($termino);
+    
+    $frecuencias{$termino}{"DF"} = $df;
+    
+    # Calculamos la cantidad de documentos con TF = 1
+    
+    if ( $frecuencias{$termino}{"TF"} == 1 ) {
+        
+        $estadisticas{"Cantidad de terminos con frecuencia 1 en la coleccion"} += 1;
+        
+    }
+    
+}
+
+$estadisticas{"Largo promedio de un termino"} = int($estadisticas{"Largo promedio de un termino"} / $estadisticas{"Cantidad de terminos extraidos"});
+
+# Busco documento mas corto y mas largo
+$estadisticas{"Cantidad de terminos del documento mas largo"} = 0;
+$estadisticas{"Cantidad de terminos del documento mas corto"} = $estadisticas{"Cantidad de terminos extraidos"};
+
+foreach $documento (keys %{$estadisticas{"cantidad_terminos_documentos"}}) {
+    
+    # Calculamos la cantidad de terminos para el documento actual
+    $cantidad_terminos_documento = scalar(keys %{$estadisticas{"cantidad_terminos_documentos"}{$documento}});
+    
+    if( $estadisticas{"Cantidad de terminos del documento mas largo"} < $cantidad_terminos_documento ) {
+        
+        $estadisticas{"Cantidad de terminos del documento mas largo"} = $cantidad_terminos_documento;
+        $estadisticas{"Nombre del documento mas largo"} = $documento;
+        
+    }
+    
+    if( $estadisticas{"Cantidad de terminos del documento mas corto"} > $cantidad_terminos_documento ) {
+        
+        $estadisticas{"Cantidad de terminos del documento mas corto"} = $cantidad_terminos_documento;
+        $estadisticas{"Nombre del documento mas corto"} = $documento;
+        
+    }
+    
+}
+
+###############
+### SALIDAS ###
+###############
+
+### TERMINOS.TXT
+
+# Abrimos el archivo de salida de terminos
+open(OUT, ">$archivo_terminos");
+
+# Teniendo procesada toda la coleccion
+foreach $termino ( sort( keys %frecuencias ) ) {
+    
+    #print OUT "Termino: $termino TF: $frecuencias{$termino}{'TF'}  DF: $frecuencias{$termino}{'DF'}\n";
+    $salida = sprintf("Termino: %-50s TF: %-5s  DF: %s\n", $termino, $frecuencias{$termino}{"TF"}, $frecuencias{$termino}{"DF"});
+    #print $salida;
+    #printf(OUT "Termino: %-50s TF: %-5s  DF: %s\n", $termino, $frecuencias{$termino}{"TF"}, $frecuencias{$termino}{"DF"});
+    print OUT $salida;
+    
+}
+
+close(OUT);
+
+### ESTADISTICAS.TXT
+
+# Abrimos el archivo
+open(OUT, ">$archivo_estadisticas");
+
+print OUT "Estadisticas de la coleccion:\n";
+print OUT "=============================\n";
+print OUT "Cantidad de documentos procesados: $estadisticas{'Cantidad de documentos procesados'}\n";
+print OUT "Cantidad de terminos extraidos: $estadisticas{'Cantidad de terminos extraidos'}\n";
+print OUT "Promedio de terminos por documento: $estadisticas{'Promedio de terminos por documento'}\n";
+print OUT "Largo promedio de un termino: $estadisticas{'Largo promedio de un termino'}\n";
+print OUT "Nombre del documento mas largo: $estadisticas{'Nombre del documento mas largo'}\n";
+print OUT "Cantidad de terminos del documento mas largo: $estadisticas{'Cantidad de terminos del documento mas largo'}\n";
+print OUT "Nombre del documento mas corto: $estadisticas{'Nombre del documento mas corto'}\n";
+print OUT "Cantidad de terminos del documento mas corto: $estadisticas{'Cantidad de terminos del documento mas corto'}\n";
+print OUT "Cantidad de terminos con frecuencia 1 en la coleccion: $estadisticas{'Cantidad de terminos con frecuencia 1 en la coleccion'}\n";
+print OUT "\n";
+
+close(OUT);
+
+### FRECUENCIAS.TXT
+
+# Abrimos el archivo
+open(OUT, ">$archivo_frecuencias");
+
+print OUT "Los 10 terminos mas frencuentes de la coleccion:\n";
+print OUT "================================================\n";
+
+for ($i=0; $i<=9; $i++)
+{   
+    $elem = @terminos_ordenados_por_frecuencias[$i];
+    print OUT "$elem: $frecuencias{$elem}{'TF'}\n";
+}
+
+print OUT "\nLos 10 terminos menos frencuentes de la coleccion:\n";
+print OUT "==================================================\n";
+
+$desde = scalar(@terminos_ordenados_por_frecuencia) - 10;
+$hasta = scalar(@terminos_ordenados_por_frecuencia) - 1;
+
+for ($i=$desde; $i<=$hasta; $i++)
+{   
+    $elem = @terminos_ordenados_por_frecuencias[$i];
+    print OUT "$elem: $frecuencias{$elem}{'TF'}\n";
+}
+
+close(OUT);
+
